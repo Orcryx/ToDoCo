abb103d47d37b3384f3bd49c423520661d6dcd2d
Orcryx <lauryannmagar@gmail.com>
2025-06-25 15:47:18 +0200

    Ajouter un voter et corrections divers

---
+++ src/Security/Voter/TaskVoter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class TaskVoter extends Voter
+{
+    public const DELETE = 'TASK_DELETE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        if (!in_array($attribute, [self::DELETE])) {
+            return false;
+        }
+        if (!$subject instanceof Task) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        // Vérifie que c'est bien une instance de ta classe User
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        /** @var Task $task */
+        $task = $subject;
+
+        return match ($attribute) {
+            self::DELETE => $this->canDelete($task, $user),
+            default => throw new \LogicException('ok!')
+        };
+    }
+
+    private function canDelete(Task $task, User $user): bool
+    {
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true) || $user === $task->getUserId()) {
+            return true;
+        }
+
+        return false;
+    }
+}
